bb90dbff3f69c3f6e804141854052bc265ae41f9
araratpoghosyan <ararat.poghosyan@gmail.com>
2018-11-27 16:02:55 +0400

    Initial commit

---
+++ manage.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import os
+import sys
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+if __name__ == '__main__':
+    load_dotenv(dotenv_path=Path('.') / '.env')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'application.settings')
+
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
